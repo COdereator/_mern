c95331c97ff5a10fe9b85a3765ea99de9b4b4d25
rohittt37 <rohitttgupta808@gmail.com>
2025-02-15 20:39:14 +0530

    book

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "../style/login.css";
+
+import loginImg from "../images/login.png";
+import userIcon from "../images/user.png";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login_left">
+        <img src={loginImg} alt="" />
+      </div>
+      <div className="login_right">
+        <div className="login_content">
+          <img src={userIcon} alt="" />
+          <h1>Login</h1>
+          <div className="login_input">
+            <input type="text" placeholder="Email" /> <br />
+            <input type="text" placeholder="Password" />
+            <br />
+            <button>Login</button>
+          </div>
+          <div className="account">
+            <span>Don't have an account? </span>
+            <NavLink to="/register">Create</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import loginImg from "../images//register.png";
+import userIcon from "../images/user.png";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="login" style={{ height: "450px" }}>
+      <div className="login_left">
+        <img src={loginImg} alt="" />
+      </div>
+      <div className="login_right">
+        <div className="login_content">
+          <img src={userIcon} alt="" />
+          <h1>Register</h1>
+          <div className="login_input">
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Email" /> <br />
+            <input type="text" placeholder="Password" />
+            <br />
+            <button>Create Account</button>
+          </div>
+          <div className="account">
+            <span>Already have an account? </span>
+            <NavLink to="/login">Login</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
